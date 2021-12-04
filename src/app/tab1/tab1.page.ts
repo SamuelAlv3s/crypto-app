6615cfa03ea81df5a3fff72418aa2e83f18aacaf
Samuel <samuelalves681@gmail>
2021-12-04 15:11:46 -0300

    buy coins

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -11,12 +11,13 @@ import { CoinService } from '../services/coin.service';
 })
 export class Tab1Page implements OnInit {
   public coins: Coin[] = null;
+  public userMoney;
   constructor(
     private coinService: CoinService,
     private toastCtrl: ToastController
   ) {}
 
-  ngOnInit() {
+  async ngOnInit() {
     this.coinService.getCoins().subscribe(async (res) => {
       console.log(res.data);
       this.coins = res.data;
@@ -35,6 +36,18 @@ export class Tab1Page implements OnInit {
         }
       }
     });
+
+    const { value } = await Storage.get({ key: 'userMoney' });
+
+    if (value) {
+      this.userMoney = JSON.parse(value);
+    } else {
+      this.userMoney = 80000;
+      await Storage.set({
+        key: 'userMoney',
+        value: JSON.stringify(this.userMoney),
+      });
+    }
   }
 
   async addCoinToFavorite(coin: Coin) {
@@ -97,6 +110,33 @@ export class Tab1Page implements OnInit {
       this.coinService.getCoins().subscribe((res) => {
         this.coins = res.data;
       });
+    }
+  }
+
+  async buyCoin(coin: Coin) {
+    const { value } = await Storage.get({ key: 'userMoney' });
+
+    if (value) {
+      let wallet = [];
+      if (JSON.parse(value) >= coin.metrics.market_data.price_usd) {
+        const { value } = await Storage.get({ key: 'wallet' });
+        if (value) {
+          wallet = JSON.parse(value);
+          wallet.push(coin);
+        } else {
+          wallet = [coin];
+        }
+
+        await Storage.set({ key: 'wallet', value: JSON.stringify(wallet) });
+        this.userMoney -= coin.metrics.market_data.price_usd;
+        await Storage.set({
+          key: 'userMoney',
+          value: JSON.stringify(this.userMoney),
+        });
+        this.showToast('Successful purchase');
+      } else {
+        this.showToast('Insufficient funds');
+      }
     }
   }
 }
