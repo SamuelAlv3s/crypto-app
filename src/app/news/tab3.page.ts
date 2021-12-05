b7a754a96d9be2fc0bb4e7ec65d5b9b30efdf225
Samuel <samuelalves681@gmail>
2021-12-05 16:14:11 -0300

    add modal details news

--- src/app/news/tab3.page.ts
+++ src/app/news/tab3.page.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { Articles, ResponseNewsApi } from '../interfaces/news.interface';
+import { ModalController } from '@ionic/angular';
+import {
+  Article,
+  Articles,
+  ResponseNewsApi,
+} from '../interfaces/news.interface';
+import { ModalNewsDetailsComponent } from '../modal-news-details/modal-news-details.component';
 import { NewsService } from '../services/news.service';
 
 @Component({
@@ -9,12 +15,24 @@ import { NewsService } from '../services/news.service';
 })
 export class Tab3Page implements OnInit {
   public news: Articles = null;
-  constructor(private newsService: NewsService) {}
+  constructor(
+    private newsService: NewsService,
+    private modalCtrl: ModalController
+  ) {}
 
   ngOnInit() {
     this.newsService.getNotices().subscribe((res: ResponseNewsApi) => {
       this.news = res.articles;
       console.log(this.news);
     });
+  }
+
+  async openNewsDetails(article: Article) {
+    const modal = await this.modalCtrl.create({
+      component: ModalNewsDetailsComponent,
+      componentProps: { article },
+    });
+
+    await modal.present();
   }
 }
